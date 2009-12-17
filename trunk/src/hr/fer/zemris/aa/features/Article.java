cbd3e655faab9b08fd6cfdc1fd640e2e72f136ce
igorbelsa <igorbelsa@967c69dc-d14f-11de-a6e7-51e3a4816e6b>
2009-12-17 17:15:25 +0000

    Dodano pravilno parsiranje datuma iz XML-a u klasu Date.
    BUG REPORT: nadjena greska u corpusu, na jednom mjesu umjesto datuma pise "prije 22 sata" :D

--- trunk/src/hr/fer/zemris/aa/features/Article.java
+++ trunk/src/hr/fer/zemris/aa/features/Article.java
@@ -1,5 +1,9 @@
 package hr.fer.zemris.aa.features;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Klasa predstavlja jedan članak određenog autora, sa svojim
  * naslovom, tekstom i datumom.
@@ -9,14 +13,37 @@ public class Article {
 	
 	private final String author;
 	private final String text;
-	private final String date;
 	private final String title;
+	private final Date date;
 	
-	public Article(String auth, String text, String title, String date){
+	private final String DATE_FORMAT = "dd.MM.yyyy";
+	
+	public Article(String auth, String text, String title, Date date){
 		this.author = auth;
 		this.text = text;
 		this.date = date;
 		this.title = title;
+	}
+	
+	//FIXME: izbaciti funkcionalnost string formatiranja datuma iz ove klase u XMLMiner
+	public Article(String auth, String text, String title, String date) {
+		this.author = auth;
+		this.text = text;
+		this.title = title;
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		
+		Date tempDate;
+		try {
+			tempDate = dateFormat.parse(date);
+		} catch (ParseException e) {
+			System.err.println("Greska kod parsiranja clanka: " + auth + " - \"" + title + "\"");
+			System.err.println("Pogresno zadan datum: " + date);
+			tempDate = new Date(1);
+			System.err.println("UPOZORENJE: datum clanka postavljen na 1.1.1970.");
+		}
+		
+		this.date = tempDate;
 	}
 
 	public String getAuthor() {
@@ -27,8 +54,13 @@ public class Article {
 		return text;
 	}
 
-	public String getDate() {
+	public Date getDate() {
 		return date;
+	}
+	
+	public String getDateString() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		return dateFormat.format(this.date);
 	}
 	
 	public String getTitle() {
