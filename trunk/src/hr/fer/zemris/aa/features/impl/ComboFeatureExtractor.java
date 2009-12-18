a1b2b4e42e73c2d9a04e844d72ce343964dc4fa7
ivan.kristo <ivan.kristo@967c69dc-d14f-11de-a6e7-51e3a4816e6b>
2009-12-18 02:12:22 +0000

    Dorada ComboFeatureExtractora. Nije nešto najljepše, ali je zasad ok. Refactoram kasnije...

--- trunk/src/hr/fer/zemris/aa/features/impl/ComboFeatureExtractor.java
+++ trunk/src/hr/fer/zemris/aa/features/impl/ComboFeatureExtractor.java
@@ -3,12 +3,41 @@ package hr.fer.zemris.aa.features.impl;
 import hr.fer.zemris.aa.features.FeatureVector;
 import hr.fer.zemris.aa.features.IFeatureExtractor;
 
+/**
+ * Feature extractor koji se sastoji od više feature extractora.
+ * 
+ * @author Ivan Krišto
+ *
+ */
 public class ComboFeatureExtractor implements IFeatureExtractor {
-
+	private IFeatureExtractor[] extractors;
+	
+	public ComboFeatureExtractor(IFeatureExtractor ... extractors) {
+		this.extractors = extractors;
+	}
+	
 	@Override
 	public FeatureVector getFeatures(String text) {
-		// TODO Auto-generated method stub
-		return null;
+		FeatureVector[] fVectors = new FeatureVector[extractors.length];
+		int sumVectorSizes = 0;
+		
+		for (int i = 0; i < extractors.length; i++) {
+			fVectors[i] = extractors[i].getFeatures(text);
+			sumVectorSizes += fVectors[i].getFeaturesDimension();
+		}
+		
+		FeatureVector comboFVector = new FeatureVector(sumVectorSizes);
+		comboFVector.describe(fVectors[0].getAuthor(), fVectors[0].getTitle());
+		
+		int cfvIndex = 0;
+		for (int i = 0; i < fVectors.length; i++) {
+			for (int j = 0; j < fVectors[i].getFeaturesDimension(); j++) {
+				comboFVector.put(cfvIndex, fVectors[i].get(j));
+				cfvIndex++;
+			}
+		}
+		
+		return comboFVector;
 	}
 
 }
