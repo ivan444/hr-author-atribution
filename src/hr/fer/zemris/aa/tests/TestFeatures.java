92819dc4dc2fc338e0741613e7729e2024b586f7
ivan.kristo <ivan.kristo@967c69dc-d14f-11de-a6e7-51e3a4816e6b>
2010-01-14 19:45:35 +0000

    Izmjena uvodne riječi o značajkama.

--- src/hr/fer/zemris/aa/tests/TestFeatures.java
+++ src/hr/fer/zemris/aa/tests/TestFeatures.java
@@ -3,9 +3,13 @@ package hr.fer.zemris.aa.tests;
 import hr.fer.zemris.aa.features.FeatureClass;
 import hr.fer.zemris.aa.features.FeatureGenerator;
 import hr.fer.zemris.aa.features.IFeatureExtractor;
-import hr.fer.zemris.aa.features.impl.FunctionWordOccurNumExtractor;
+import hr.fer.zemris.aa.features.impl.ComboFeatureExtractor;
+import hr.fer.zemris.aa.features.impl.FunctionWordTFIDFExtractor;
+import hr.fer.zemris.aa.features.impl.PunctuationMarksExtractor;
+import hr.fer.zemris.aa.features.impl.VowelsExtractor;
 import hr.fer.zemris.aa.xml.XMLMiner;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 
@@ -25,7 +29,12 @@ public class TestFeatures {
 		
 		XMLMiner miner = new XMLMiner(args[0]);
 		//IFeatureExtractor extractor = new SimpleFeatureExtractor(new File("config/fwords.txt"));
-		IFeatureExtractor extractor2 = new FunctionWordOccurNumExtractor("config/fwords.txt");
+		//IFeatureExtractor extractor2 = new FunctionWordOccurNumExtractor("config/fwords.txt");
+		IFeatureExtractor extractor2 = new ComboFeatureExtractor(
+				new PunctuationMarksExtractor(new File("config/marks.txt")),
+				new VowelsExtractor(),
+				new FunctionWordTFIDFExtractor("config/fw-idf.txt")
+		);
 		
 		FeatureGenerator generator = new FeatureGenerator(extractor2);
 		
